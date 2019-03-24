56bb336f49e804435d4d40f85742a5a18e14b0c1
sawyermade <sawyermade0@gmail.com>
2019-03-23 20:22:09 -0400

    lazy push

--- run_pipe1.sh
+++ run_pipe1.sh
@@ -5,4 +5,4 @@ sudo /opt/anaconda3/envs/py3.6-pyt1.0.1/bin/python pipe1_detectron.py \
 	--image-ext jpg \
 	--wts ./models/e2e_mask_rcnn_R-101-FPN_2x.pkl \
 	--output-ext png \
-	--thresh 0.1
+	--thresh 0.3
