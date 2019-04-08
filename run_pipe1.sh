07e5a3ac83e7bc8679826fa7251324f62208c83a
Daniel Sawyer <sawyermade0@gmail.com>
2019-04-08 17:30:29 -0400

    lazy push

--- run_pipe1.sh
+++ run_pipe1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo /opt/anaconda3/envs/py3.6-pyt1.0.1/bin/python pipe1_detectron.py \
+sudo /home/anaconda3/envs/pyt1.0.1/bin/python pipe1_detectron.py \
 	--cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
 	--output-dir ./uploads-pipe1 \
 	--image-ext jpg \
